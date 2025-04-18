ea8d2fecfffc8b0dc23944cb640e816af78264c6
pothedev <milka.ev3@gmail.com>
2025-04-18 18:42:05 +0300

    upd

--- functions/firebase_config.py
+++ functions/firebase_config.py
@@ -2,12 +2,17 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Get the raw JSON string from env
 firebase_config_json = os.getenv("FIREBASE_CONFIG_JSON")
 
 # Convert it to a dictionary
 firebase_dict = json.loads(firebase_config_json)
+firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n")
+
 
 # Initialize Firebase only once
 if not firebase_admin._apps:
