17f68d8e4a26feb76ec628c005ea5fb61564c836
pothedev <milka.ev3@gmail.com>
2025-05-11 22:05:26 +0300

    upd

--- functions/keepalive.py
+++ functions/keepalive.py
@@ -7,5 +7,10 @@ app = Flask('')
 def home():
     return "Pluto bot is live", 200
 
+@app.route('/ping')
+def ping():
+    return "pong", 200
+
+
 def keep_alive():
     Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
